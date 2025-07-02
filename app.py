44f322a46ac55d0627748e33d243304aee4f7345
sakshi wakhare <wakharesakshi2@gmail.com>
2025-07-02 21:41:40 +0530

    Added bonus task: PR merge detection

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
-    print("Received webhook data:", data)
+    if data.get("action") == "closed" and data.get("pull_request", {}).get("merged"):
+        print("A Pull Request was merged!")
     return jsonify({"status": "success"}), 200
+
 
 if __name__ == "__main__":
     app.run(port=5000)
